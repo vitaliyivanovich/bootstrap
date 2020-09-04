359ec255e9e5489825c09bb1c96fed4cd3626782
Vitaliy <vitaliydiscord@gmail.com>
2020-09-04 15:48:49 +0300

    JS adaptive fix

--- assets/build/js/main.js
+++ assets/build/js/main.js
@@ -21725,45 +21725,46 @@ return Popper;
 //import working JS
 
 $('.slider').slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    prevArrow: false,
-    nextArrow: false,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    responsive: [
-      {
-        breakpoint: 850,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-    ]
-  });
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  prevArrow: false,
+  nextArrow: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 850,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+  ]
+});
 
-$('#nav-icon3').on('click',function(){
+$('#nav-icon3').on('click', function () {
   $('header .container .navbar nav').toggleClass('shown');
   $('header .nav_container').toggleClass('black');
   $(this).toggleClass('open');
 });
 
-$(window).resize(function() {
-  if ($(window).width() > 991 ) {
+$(window).resize(function () {
+  if ($(window).width() > 991) {
     $('header .container .navbar nav').removeClass('shown');
     $('header .nav_container').removeClass('black');
     $('#nav-icon3').removeClass('open');
   }
 });
-if ($(window).width() < 991 ) {
-  $('header .container .navbar nav ul li a').click(function() {
+
+$('header .container .navbar nav ul li a').click(function () {
+  if ($(window).width() < 991) {
     $('header .container .navbar nav').removeClass('shown');
     $('header .nav_container').removeClass('black');
     $('#nav-icon3').removeClass('open');
-  });
-}
+  }
+});
